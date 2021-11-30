c33eecd554f498f6279d6d5ae7670d58b338435d
мариялевицкая <95248309+2bvt21010@users.noreply.github.com>
2021-11-30 18:03:16 +0300

    Add files via upload

---
+++ 11.py
@@ -0,0 +1,7 @@
+from math import pi, tan
+print("#1")
+s=float(input("S="))
+n=int(input("N="))
+out=n*s**2/(4*tan(pi/n))
+print(out)
+input()
---
+++ 12.py
@@ -0,0 +1,5 @@
+print("#2")
+n=int(input("N="))
+out=n*(n+1)/2
+print(out)
+input() 
